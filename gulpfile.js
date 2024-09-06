9ad179f860cacd21dd7e2e9e1a919d80d5dcff58
Nakuromu <pavlokordunov@gmail.com>
2024-09-06 22:09:43 +0300

    final settings

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,9 @@ const gulp = require('gulp');
 const fileInclude = require('gulp-file-include');
 const sass = require('gulp-sass')(require('sass'));
 const server = require('gulp-server-livereload');
+const clean = require('gulp-clean');
+const fs = require('fs');
+const webpack = require('webpack-stream');
 
 const fileIncludeSettings = {
     prefix: '@@',
@@ -34,4 +37,30 @@ const serverSettings = {
 
 gulp.task('start', function(){
     return gulp.src('./dist/').pipe(server(serverSettings))
+})
+
+gulp.task('clean', function(done){
+    if(fs.existsSync('./dist/')){
+        return gulp.src('./dist/').pipe(clean())
+    }
+
+    done();
+})
+
+gulp.task('watch', function(){
+    gulp.watch('./src/sass/**/*.scss', gulp.parallel('sass'));
+    gulp.watch('./src/**/*.html', gulp.parallel('html'));
+    gulp.watch('./src/img/**/*', gulp.parallel('img'));
+})
+
+gulp.task('default', gulp.series(
+    'clean',
+    gulp.parallel('html', 'sass', 'img'),
+    gulp.parallel('start', 'watch'),
+))
+
+gulp.task('js', function(){
+    return gulp.src('./src/js/*.js')
+        .pipe(webpack(require('./webpack.config.js')))
+        .pipe(gulp.dest('./dist/js'))
 })
